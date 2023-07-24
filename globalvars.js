dad1674a1296775b56e58e1af82959c9f7940130
Vivek Kogilathota <vkogilathota@gmail.com>
2023-07-24 10:56:58 -0500

    docs(url): created global config to manage urls #25
    created global config file to manage rubicon urls throughout the mdx files

---
+++ globalvars.js
@@ -0,0 +1,8 @@
+export const siteUrls = {
+   baseUrl: "https://app.rubicon.finance/",
+   githubProtocolUrl: "https://github.com/RubiconDeFi/rubi-protocol-v2",
+   discordUrl: "https://discord.com/invite/E7pS24J",
+   duneUrl: "https://dune.com/rubicondefi/rubicon",
+   bugBountyUrl: "https://immunefi.com/bounty/rubicon/",
+
+}
