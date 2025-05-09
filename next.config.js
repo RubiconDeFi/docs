51c872bc6fcfbc95e40fb74ab40d274f20f7ecdc
daoio <albyullov@gmail.com>
2025-05-09 12:45:23 +0300

    fix: rm incorrect sidebar structure from config

--- next.config.js
+++ next.config.js
@@ -9,19 +9,8 @@ const withNextra = require("nextra")({
 
 module.exports = withNextra({
 	i18n: {
-		locales: ["en", "vi", "zh", "hi", "es", "fr", "ar", "ru", "ja", "ko", "fa", "id"],
+	        // English-only, for now.
+		locales: ["en"/*, "vi", "zh", "hi", "es", "fr", "ar", "ru", "ja", "ko", "fa", "id"*/],
 		defaultLocale: "en",
 	},
-	sidebar: {
-		items: [
-		  { title: 'Overview', href: '/' },
-		  { title: 'FAQ', href: '/faq' },
-		  { title: 'Protocol', href: '/protocol' },
-		  { title: 'API Guides', href: '/api-guides' },
-		  { title: 'Market Making', href: '/market-making' },
-		  { title: 'Guides', href: '/guides' },
-		  { title: 'Rubicon Liquidity Program', href: '/rubiconLiquidityProgram' },
-		  { title: 'Rubicon RFQ', href: '/rubiconRfq' },
-		],
-	  },
 });
